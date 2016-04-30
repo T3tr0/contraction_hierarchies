5dd845767b36d7411ebfa01778cecd3a31204aa7
Thomas Ngo Trung <thomas.ngotrung@gmail.com>
2016-04-30 15:29:36 +0200

    ArchPathFromSourceTo OK

--- dijkstra.cc
+++ dijkstra.cc
@@ -27,30 +27,34 @@ void Dijkstra::Run(int source) {
     vector<int> tmp_arcs = graph_.OutgoingArcs(p.node);
     // cout << "***** arcs from source : " << tmp_arcs.size() << "*****" << endl << endl;
     for (auto arc: tmp_arcs) {
-      cout << "########" <<endl;
+      // cout << "########" <<endl;
       // cout << "source: " << p.node << endl;
       // cout << "arc: " << arc << endl;
       // cout << "arc tail: " << graph_.Tail(arc) << endl;
       // cout << "arc head: " << graph_.Head(arc) << endl;
       // cout << "arc length: " << arc_lengths_[arc] << endl << endl;
 
-      int node_dest = p.node == graph_.Head(arc) ? graph_.Tail(arc) : graph_.Head(arc);
-      //  cout << "node_dest: " << node_dest << endl;
-      //  cout << "node_dest distance: " << distance_[node_dest] << endl << endl;
+      int arc_node_to = p.node == graph_.Head(arc) ? graph_.Tail(arc) : graph_.Head(arc);
+      //  cout << "arc_node_to: " << arc_node_to << endl;
+      //  cout << "arc_node_to distance: " << distance_[arc_node_to] << endl << endl;
 
-      if (distance_[node_dest] ==  std::numeric_limits<double>::infinity()
-      || distance_[node_dest] > p.distance + arc_lengths_[arc]) {
+      if (distance_[arc_node_to] ==  std::numeric_limits<double>::infinity()
+      || distance_[arc_node_to] > p.distance + arc_lengths_[arc]) {
         // cout << "### update dist and parent ###" << endl;
-        distance_[node_dest] = p.distance + arc_lengths_[arc];
-        // cout << "node_dest distance: " << distance_[node_dest] << endl;
-        parent_arc_[node_dest] = arc;
-        // cout << "parent_arc:" << parent_arc_[node_dest] << endl;
-        DijkstraState state = {node_dest, distance_[node_dest]};
+        distance_[arc_node_to] = p.distance + arc_lengths_[arc];
+        // cout << "arc_node_to distance: " << distance_[arc_node_to] << endl;
+        parent_arc_[arc_node_to] = arc;
+        // cout << "parent_arc:" << parent_arc_[arc_node_to] << endl;
+        DijkstraState state = {arc_node_to, distance_[arc_node_to]};
         //cout << "new DijkstraState: " << state.node << ", " << state.distance << endl << endl;
         pq_.push(state);
       }
     }
   }
+  cout << "###### ARC PATH #####" << endl;
+  cout << "node source: " << source << endl;
+  ArcPathFromSourceTo(5);
+  cout << endl;
 }
 
 const vector<double>& Dijkstra::Distances() const {
@@ -62,8 +66,18 @@ const vector<int>& Dijkstra::ParentArcs() const {
 }
 
 vector<int> Dijkstra::ArcPathFromSourceTo(int node) const {
-  vector<int> tmp;
-  return tmp;
+  vector<int> res;
+  int arc = parent_arc_[node];
+  int prev = node;
+  cout << prev << " -> " ;
+  while (arc != -1) {
+    prev = (prev == graph_.Tail(arc)) ? graph_.Head(arc) : graph_.Tail(arc);
+    res.push_back(arc);
+    cout << prev << " -> " ;
+    arc = parent_arc_[prev];
+  }
+  cout << "ARRIVED" << endl;
+  return res;
 }
 
 /*
